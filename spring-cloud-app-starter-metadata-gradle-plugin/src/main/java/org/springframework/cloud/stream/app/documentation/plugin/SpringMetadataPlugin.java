8ed851153c5bb926a488a5f1348bb932eb402a60
Alexander Furer <alexander.furer@philips.com>
2018-01-31 17:29:34 +0200

    introducing spring  metadata gradle plugin

---
+++ spring-cloud-app-starter-metadata-gradle-plugin/src/main/java/org/springframework/cloud/stream/app/documentation/plugin/SpringMetadataPlugin.java
@@ -0,0 +1,27 @@
+package org.springframework.cloud.stream.app.documentation.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.plugins.JavaPlugin;
+
+public class SpringMetadataPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        SpringMetadataTask metadataTask = project.getTasks().create(SpringMetadataTask.NAME, SpringMetadataTask.class);
+
+
+
+        project.afterEvaluate(p->{
+            Task buildTask = p.getTasks().findByName("build");
+            if(null!=buildTask){
+                buildTask.dependsOn(metadataTask);
+            }
+            p.getArtifacts().add("archives",metadataTask);
+
+            if(p.getPlugins().hasPlugin(JavaPlugin.class)){
+                metadataTask.dependsOn(JavaPlugin.COMPILE_JAVA_TASK_NAME);
+            }
+        });
+    }
+}
